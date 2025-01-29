2b1a27742e6b170948183fa5b174d7279281ee1a
Jamie Buick <jamiebuick28@gmail.com>
2025-01-29 22:08:35 +0000

    Initial commit for the OOSD module of ATU HDip

---
+++ Hello/src/ie/atu/sw/Hello.java
@@ -0,0 +1,14 @@
+package ie.atu.sw;
+
+public class Hello {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+
+		
+
+	}
+}
+
+
+
---
+++ Student/src/ie/atu/sw/Student.java
@@ -0,0 +1,21 @@
+package ie.atu.sw;
+
+public class Student {
+	
+	/*
+	 * This is how we initialise the Student
+	 * in Java.
+	 * 
+	 */
+	public Student() {
+		System.out.println(this);
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		new Student ();	// Create a new student object
+		
+	}
+
+}
