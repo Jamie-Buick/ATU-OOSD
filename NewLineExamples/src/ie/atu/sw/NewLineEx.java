8a4eecde86b20b6c4dff2ae25ffbd957cd6bc124
Jamie Buick <jamiebuick28@gmail.com>
2025-02-09 16:58:21 +0000

    same example, just a different way to do it using multiple methods rather than 1

--- NewLineExamples/src/ie/atu/sw/NewLineEx.java
+++ NewLineExamples/src/ie/atu/sw/NewLineEx.java
@@ -4,25 +4,25 @@ public class NewLineEx {
 
 	public static void main(String[] args) {
 		System.out.println("First Line.");
-		//newLine();
+		
 		threeLines();
 		System.out.println("Second Line.");
 	}
 	
 	
-	//
-	/*
+	
+	
 	public static void newLine() {
 		System.out.println("");
 	}
 	
-	*/
+	
 	
 	
 	public static void threeLines() {
-		System.out.println("");
-		System.out.println("");
-		System.out.println("");
+		newLine();
+		newLine();
+		newLine();
 	}
 	
 }
