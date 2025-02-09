88a7393e30580b842dc49653f45773b78aa38102
Jamie Buick <jamiebuick28@gmail.com>
2025-02-09 16:55:15 +0000

    methods example from book, basic printing lines in a method and then calling in the main

---
+++ NewLineExamples/src/ie/atu/sw/NewLineEx.java
@@ -0,0 +1,28 @@
+package ie.atu.sw;
+
+public class NewLineEx {
+
+	public static void main(String[] args) {
+		System.out.println("First Line.");
+		//newLine();
+		threeLines();
+		System.out.println("Second Line.");
+	}
+	
+	
+	//
+	/*
+	public static void newLine() {
+		System.out.println("");
+	}
+	
+	*/
+	
+	
+	public static void threeLines() {
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+	}
+	
+}
