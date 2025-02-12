f62b798e3199d11965df93437ec7ce58757c674c
Jamie Buick <jamiebuick28@gmail.com>
2025-02-12 20:52:05 +0000

    Example completed from lecture regarding classes and objects

--- ClassesAndObjects/src/ie/atu/sw/Runner.java
+++ ClassesAndObjects/src/ie/atu/sw/Runner.java
@@ -1,8 +1,32 @@
 package ie.atu.sw;
 
+import java.time.LocalDate;
+
 public class Runner {
 
 	public static void main(String[] args) {
+		/*
+		 *  The new keyword creates an instance of a class. An
+		 *  instance of a class is called an object.
+		 */
+		Student s = new Student(); // use the new keyword against a constructor
+		System.out.println("Student from Runner: " + s);
 		
+		// Use the dot notation to access features
+		s.sid = 99999;
+		s.name = "Jamie Buick";
+		s.dob = LocalDate.now();
+		
+		
+		// I can also use the dot notation to invoke methods
+		s.register();
+		s.study();
+		s.execute();
+		
+		// Access the state
+		System.out.println(s.sid);
+		System.out.println(s.name);
+		System.out.println(s.dob);
+		System.out.println(s.registered);
 	}
 }
--- ClassesAndObjects/src/ie/atu/sw/Student.java
+++ ClassesAndObjects/src/ie/atu/sw/Student.java
@@ -1,5 +1,34 @@
 package ie.atu.sw;
 
-public class Student {
+import java.time.LocalDate;
 
+
+public class Student {
+/*
+ * Define some state for the class Student. These are
+ * all **instance variables**
+ */
+	
+	public long sid;
+	public String name = "Undefined";
+	public LocalDate dob;				//LocalDate seems to be more up to date than util.
+	public boolean registered = false;
+	
+	public Student() {
+		System.out.println("The ID of Student the Object is " + this);
+	}
+	
+	// Add some methods - things a class can **do** (behaviours)
+	public void register() {
+		this.registered = true;
+	}
+	
+	public void study() {
+		System.out.println("Studying...");
+	}
+	
+	public void execute() {
+		System.out.println("Execute");
+	}
+	
 }
