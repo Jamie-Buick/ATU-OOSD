8810eefc3188b10e5836ca6ed6663e82b515f2d7
Jamie Buick <jamiebuick28@gmail.com>
2025-02-16 20:58:25 +0000

    Fermat's last theorm exercise

---
+++ Ex4_5/src/ie/atu/sw/FermatsLastTheorem.java
@@ -0,0 +1,50 @@
+package ie.atu.sw;
+
+import java.lang.Math;
+
+
+public class FermatsLastTheorem {
+
+	public static void checkFermat(int a, int b, int c, int n) {
+		a = raiseToPow(a,n);
+		b = raiseToPow(b,n);
+		c = raiseToPow(c,n);
+		
+		//System.out.println(a);
+		//System.out.println(b);
+		//System.out.println(c);
+		
+		int ab = a + b;
+		
+		if (n > 2) 
+		{
+			if (ab == c)
+			{
+				System.out.println("Holy smokes, Fermat was wrong!");
+			}
+			else 
+			{
+				System.out.println("No, that doesn't work");
+			}
+		}
+		else
+		{
+			System.out.println("Fermat’s theorem only applies for n > 2.");
+		}
+
+		
+	}
+	
+	public static int raiseToPow(int x, int y) {
+		int z = (int) Math.pow(x, y);
+		
+		return z;
+	}
+	
+	public static void main(String[] args) {
+		
+		checkFermat(1,1,2,3);
+		
+		
+	}
+}
