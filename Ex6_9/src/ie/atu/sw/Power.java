095adf14c222e8850a9fef70707be22943fa11cb
Jamie Buick <jamiebuick28@gmail.com>
2025-03-07 18:42:07 +0000

    eX6.9

---
+++ Ex6_9/src/ie/atu/sw/Power.java
@@ -0,0 +1,33 @@
+package ie.atu.sw;
+
+public class Power {
+
+	
+	public static double power(double x, int n) {
+		
+		
+		
+		if(n == 0) 
+		{
+			return 1;
+		}
+		else if(n > 0)
+		{
+			return x * power(x, n-1);
+		}
+		else
+		{
+			return 1 / power(x,-n); 
+		}
+		
+		
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		double ans = power(2, 2);
+		System.out.println(ans);
+		
+	}
+}
