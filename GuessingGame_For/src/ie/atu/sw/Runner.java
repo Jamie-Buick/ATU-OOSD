6b12e90803a4f19d0db60860846ce3393116ecc7
Jamie Buick <jamiebuick28@gmail.com>
2025-03-27 19:53:50 +0000

    Created a new project to replace while loop with a for loop for guessing game

---
+++ GuessingGame_For/src/ie/atu/sw/Runner.java
@@ -0,0 +1,9 @@
+package ie.atu.sw;
+
+public class Runner {
+
+	public static void main(String[] args) {
+		// New instance of our class guessing game & calling start.
+		new NumberGuessingGame().start();
+	}
+}
