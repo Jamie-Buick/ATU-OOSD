cb9e98f215223d7fcf98b1b5588ef6959f6f03ff
Jamie Buick <jamiebuick28@gmail.com>
2025-04-22 15:33:21 +0100

    Week 14 Caesar Cipher code

---
+++ CaesarCipher/src/ie/atu/sw/CaesarCipher.java
@@ -0,0 +1,40 @@
+package ie.atu.sw;
+
+public class CaesarCipher {
+
+	
+	private int key = 0;
+
+	
+	public CaesarCipher(int key) {
+		this.key = key;
+	}
+	
+	public String encrypt(String plainText) {
+		String enc = "";
+		
+		for (int i = 0; i < plainText.length(); i++) 
+		{
+			char ch = (char) (plainText.codePointAt(i) + key);
+			enc = enc + ch;
+		}
+		
+		return enc;
+	}
+	
+	public String decrypt(String cipherText) {
+		
+		String dec = "";
+		
+		for (int i = 0; i < cipherText.length(); i++) 
+		{
+			char ch = (char) (cipherText.codePointAt(i) - key);
+			dec = dec + ch;
+		}
+			
+		return dec;
+	}
+	
+	
+	
+}
---
+++ CaesarCipher/src/ie/atu/sw/Runner.java
@@ -0,0 +1,13 @@
+package ie.atu.sw;
+
+public class Runner {
+
+	
+	public static void main(String[] args) {
+		CaesarCipher cc = new CaesarCipher(1);
+		
+		String encrypted = cc.encrypt("Gauls are smelly and dangerous");
+		System.out.println(encrypted);
+		System.out.println(cc.decrypt(encrypted));
+	}
+}
