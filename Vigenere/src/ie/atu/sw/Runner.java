36dd6ec6ed3307eef207be6fc42d96ce4c7ef4cc
Jamie Buick <jamiebuick28@gmail.com>
2025-04-28 21:28:36 +0100

    Vigenere cipher from week 15 lab

---
+++ Vigenere/src/ie/atu/sw/Runner.java
@@ -0,0 +1,15 @@
+package ie.atu.sw;
+
+public class Runner {
+
+	
+	public static void main(String[] args) {
+		String key = "THEQUICKBROWNFOXJUMPEDOVERTHELAZYDOGS";
+		String plainText = "ATTACK THE CASTLE WALL AT DAWN";
+		
+		Vigenere cipher = new Vigenere(key);
+		String cipherText = cipher.encrypt(plainText);
+		System.out.println(cipherText);
+		System.out.println(cipher.decrypt(cipherText));
+	}
+}
---
+++ Vigenere/src/ie/atu/sw/Vigenere.java
@@ -0,0 +1,115 @@
+package ie.atu.sw;
+
+public class Vigenere {
+
+	private char[] key = null;
+	
+	public Vigenere(String key) {
+	
+		setKey(key);
+	}
+	
+	public void setKey(String key) {
+		this.key = key.trim().toUpperCase().toCharArray();
+	}
+
+	
+	
+	public String encrypt(String plainText) {
+		StringBuilder sb = new StringBuilder();
+		
+		for (int i = 0; i < plainText.length(); i++) {
+			sb.append(getEncryptedCharacter(key[i], plainText.charAt(i)));
+		}
+		
+		return sb.toString();
+	}
+	
+	
+	private char getEncryptedCharacter(char key, char plain) {
+		
+		for (int row = 0; row < TABULA_RECTA.length; row++ ) {
+			
+			if(TABULA_RECTA[row][0] == key)
+			{
+				for (int col = 0; col < TABULA_RECTA[row].length; col++ ) {
+					
+					if(TABULA_RECTA[0][col] == plain)
+					{
+						return TABULA_RECTA[row][col];
+					}
+				}
+			}
+		}
+		return plain;
+	}
+	
+	
+	
+	
+	public String decrypt(String cipherText) {
+		StringBuilder sb = new StringBuilder();
+		
+		for (int i = 0; i < cipherText.length(); i++) {
+			sb.append(getDecryptedCharacter(key[i], cipherText.charAt(i)));
+		}
+		
+		return sb.toString();
+	}
+	
+	
+	private char getDecryptedCharacter(char key, char cipher) {
+		
+		for (int col = 0; col < TABULA_RECTA[0].length; col++ )
+		{
+			if(TABULA_RECTA[0][col] == key)
+			{
+				for (int row = 0; row < TABULA_RECTA.length; row++) 
+				{
+					if (TABULA_RECTA[row][col] == cipher)
+					{
+						return TABULA_RECTA[row][0];
+					}
+				}
+			}
+		}
+		
+		
+		
+		return cipher;
+	}
+	
+	
+	
+	
+	
+	private static final char[][] TABULA_RECTA = { 
+			{'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'},
+			{'B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','A'},
+			{'C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','A','B'},
+			{'D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','A','B','C'},
+			{'E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','A','B','C','D'},
+			{'F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','A','B','C','D','E'},
+			{'G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','A','B','C','D','E','F'},
+			{'H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','A','B','C','D','E','F','G'},
+			{'I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','A','B','C','D','E','F','G','H'},
+			{'J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','A','B','C','D','E','F','G','H','I'},
+			{'K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','A','B','C','D','E','F','G','H','I','J'},
+			{'L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','A','B','C','D','E','F','G','H','I','J','K'},
+			{'M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','A','B','C','D','E','F','G','H','I','J','K','L'},
+			{'N','O','P','Q','R','S','T','U','V','W','X','Y','Z','A','B','C','D','E','F','G','H','I','J','K','L','M'},
+			{'O','P','Q','R','S','T','U','V','W','X','Y','Z','A','B','C','D','E','F','G','H','I','J','K','L','M','N'},
+			{'P','Q','R','S','T','U','V','W','X','Y','Z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O'},
+			{'Q','R','S','T','U','V','W','X','Y','Z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P'},
+			{'R','S','T','U','V','W','X','Y','Z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q'},
+			{'S','T','U','V','W','X','Y','Z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R'},
+			{'T','U','V','W','X','Y','Z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S'},
+			{'U','V','W','X','Y','Z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T'},
+			{'V','W','X','Y','Z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U'},
+			{'W','X','Y','Z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V'},
+			{'X','Y','Z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W'},
+			{'Y','Z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X'},
+			{'Z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y'}
+		};
+	
+}
